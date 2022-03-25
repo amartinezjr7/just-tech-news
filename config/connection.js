cf0b993a8cf74b888dc820da55189cf56fac6676
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-03-24 20:53:57 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,22 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+const password = 'qsxdrfvgy8426!@#/*-$';
+
+let sequelize;
+
 //create caonnection to our db
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'qsxdrfvgy8426!@#/*-$', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else{
+    sequelize = new Sequelize(process.env.just_tech_news_db, process.env.root, process.env.password,{
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+        
+}
+
 
 module.exports = sequelize;
