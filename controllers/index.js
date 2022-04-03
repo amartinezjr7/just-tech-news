659d3c9271bb4c891df82496ca9dbc0c177197cf
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-04-03 15:50:26 -0700

    completed login

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,5 @@ const apiRoutes = require('./api');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 module.exports = router;
